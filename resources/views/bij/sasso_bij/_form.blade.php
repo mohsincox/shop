534a44e5a6810e5db961309ab8b78dfaee4e6f3f
mohsin <mohsin.cse.45@gmail.com>
2016-08-29 19:38:15 +0600

    menu setting in bangla

--- resources/views/bij/sasso_bij/_form.blade.php
+++ resources/views/bij/sasso_bij/_form.blade.php
@@ -1,19 +1,19 @@
 {!! Form::open(['url' => 'sasso-bij', 'method' => 'post', 'enctype' => 'multipart/form-data']) !!}
-
-<div class="form-group" {{ $errors->has('name') ? 'has error' : '' }}>
+<div class="form-horizontal">
+<div class="required form-group" {{ $errors->has('name') ? 'has error' : '' }}>
     {!! Form::label('name', 'নাম', ['class' => 'col-sm-3 control-label']) !!}
-    <div class="col-sm-9">
-        {!! Form::text('name', null,['class' => 'form-control', 'placeholder' => 'নাম', 'autocomplete' => 'off']) !!}
+    <div class="col-sm-9 required">
+        {!! Form::text('name', null,['class' => 'form-control', 'placeholder' => 'নাম', 'autocomplete' => 'off', 'required']) !!}
         <span class="help-block text-danger">
             {{ $errors->first('name') }}
         </span>
     </div>
 </div>
 
-<div class="form-group" {{ $errors->has('code') ? 'has error' : ''}}>
+<div class="form-group required" {{ $errors->has('code') ? 'has error' : ''}}>
     {!! Form::label('code', 'কোড ', ['class' => 'col-sm-3 control-label']) !!}
     <div class="col-sm-9">
-        {!! Form::text('code', null, ['class' => 'form-control', 'placeholder' => 'কোড ', 'autocomplete' => 'off']) !!}
+        {!! Form::text('code', null, ['class' => 'form-control', 'placeholder' => 'কোড ', 'autocomplete' => 'off', 'required']) !!}
         <span class="help-block text-danger">
             {{ $errors->first('code') }}
         </span>
@@ -23,7 +23,7 @@
 <div class="form-group" {{ $errors->has('quantity') ? 'has error' : '' }}>
     {!! Form::label('quantity', 'পরিমান', ['class' => 'col-sm-3 control-label']) !!}
     <div class="col-sm-9">
-        {!! Form::number('quantity', null, ['class' => 'form-control', 'placeholder' => 'পরিমান', 'autocomplete' => 'off']) !!}
+        {!! Form::text('quantity', null, ['class' => 'form-control', 'placeholder' => 'পরিমান', 'autocomplete' => 'off', 'required']) !!}
         <span class="help-block text-danger">
             {{ $errors->first('quantity') }}
         </span>
@@ -33,7 +33,7 @@
 <div class="form-group" {{ $errors->has('price') ? 'has error' : '' }}>
     {!! Form::label('price', 'মূল্য', ['class' => 'col-sm-3 control-label']) !!}
     <div class="col-sm-9">
-        {!! Form::number('price', null, ['class' => 'form-control', 'placeholder' => 'মূল্য', 'autocomplete' => 'off']) !!}
+        {!! Form::text('price', null, ['class' => 'form-control', 'placeholder' => 'মূল্য', 'autocomplete' => 'off', 'required']) !!}
         <span class="help-block text-danger">
             {{ $errors->first('price') }}
         </span>
@@ -43,7 +43,7 @@
 <div class="form-group" {{ $errors->has('description') ? 'has error' : '' }}>
     {!! Form::label('description', 'ব্যাখ্যা', ['class' => 'col-sm-3 control-label']) !!}
     <div class="col-sm-9">
-        {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'ব্যাখ্য', 'autocomplete' => 'off', 'rows' => 4]) !!}
+        {!! Form::textarea('description', null, ['class' => 'form-control', 'placeholder' => 'ব্যাখ্য', 'autocomplete' => 'off', 'rows' => 4, 'required']) !!}
         <span class="help-block text-danger">
             {{ $errors->first('description') }}
         </span>
@@ -53,13 +53,19 @@
 <div class="form-group" {{ $errors->has('image') ? 'has error' : '' }}>
     {!! Form::label('image', 'ছবি', ['class' => 'col-sm-3 control-label']) !!}
     <div class="col-sm-9">
-        {!! Form::file('image', ['class' => 'form-control', 'onchange' => 'readURL(this)']) !!}
+        {!! Form::file('image', ['class' => 'form-control', 'onchange' => 'readURL(this)', 'required']) !!}
     </div>
-    {{ Html::image('#', 'your image',['id' => 'blah']) }}
+    <span class="help-block">
+        {{ $errors->first('image') }}
+    </span>
+    <div class="col-sm-offset-3">
+        {{ Html::image('#', 'your image',['id' => 'blah']) }}
+    </div>
 </div>
 
 <div class="form-group">
-    {!! Form::submit('SUBMIT', ['class' => 'btn btn-primary']) !!}
+    {!! Form::submit('SUBMIT', ['class' => 'btn btn-primary col-sm-offset-3']) !!}
+</div>
 </div>
 
 {!! Form::close() !!}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,30 +64,54 @@
 
                     <ul class="nav navbar-nav">
 
-                        <li><a class="is-active" href="index.html">শস্য বীজ</a></li>
-                        <li>{!! Html::link('indec', 'শস্য বীজ') !!}</li>
-                        <li class="has-child"><a href="#">বীজ</a>
+
+                        <li>{!! Html::link('/', 'হোম ') !!}</li>
+                        <li class="has-child">{!! Html::link('#', 'বীজ') !!}
 
                             <ul class="submenu">
-                                <li class="submenu-item"><a href="causes.html">শস্য বীজ</a></li>
-                                <li class="submenu-item"><a href="causes-single.html">শস্য বীজ </a></li>
-                                <li class="submenu-item"><a href="causes-single.html">শস্য বীজ </a></li>
-                                <li class="submenu-item"><a href="causes-single.html">শস্য বীজ </a></li>
+                                <li class="submenu-item">{!! Html::link('#', 'শস্য বীজ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'শাকসবজি বীজ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'ফুল বীজ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'ফলজ বীজ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'ঔষধি বীজ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'বনজ বীজ') !!}</li>
                             </ul>
 
                         </li>
-                        <li class="has-child"><a href="#">CAUSES</a>
+                        <li class="has-child">{!! Html::link('#', 'ঔষধ') !!}
 
                             <ul class="submenu">
-                                <li class="submenu-item"><a href="causes.html">Causes list </a></li>
-                                <li class="submenu-item"><a href="causes-single.html">Single cause </a></li>
-                                <li class="submenu-item"><a href="causes-single.html">Single cause </a></li>
-                                <li class="submenu-item"><a href="causes-single.html">Single cause </a></li>
+                                <li class="submenu-item">{!! Html::link('#', 'গবাদি পশুর ঔষধ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'হাঁস-মুরগীর ঔষধ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'মৎস ঔষধ') !!}</li>
                             </ul>
 
                         </li>
-                        <li><a href="gallery.html">GALLERY</a></li>
-                        <li><a href="contact.html">CONTACT</a></li>
+                        <li>{!! Html::link('#', 'কিটনাশক') !!}</li>
+                        <li class="has-child">{!! Html::link('#', 'খাবার/খাদ্য') !!}
+
+                            <ul class="submenu">
+                                <li class="submenu-item">{!! Html::link('#', 'গবাদি পশুর খাদ্য') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'হাঁস-মুরগীর খাদ্য') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'কৃষি খাদ্য') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'মৎস খাদ্য') !!}</li>
+                            </ul>
+
+                        </li>
+                        <li class="has-child">{!! Html::link('#', 'নার্সারী') !!}
+
+                            <ul class="submenu">
+                                <li class="submenu-item">{!! Html::link('#', 'ফুল') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'ফল') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'ঔষধি') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'বনজ') !!}</li>
+                                <li class="submenu-item">{!! Html::link('#', 'ক্যাকটাস') !!}</li>
+                            </ul>
+
+                        </li>
+                        <li>{!! Html::link('#', 'যন্ত্রপাতি') !!}</li>
+                        <li>{!! Html::link('#', 'উপকরণ') !!}</li>
+                        <li>{!! Html::link('#', 'ঠিকানা/যোগাযোগ  ') !!}</li>
 
                     </ul>
 
@@ -293,31 +317,7 @@
 
 
 
-{{--<div class="section-home our-sponsors animate-onscroll fadeIn">--}}
 
-{{--<div class="container">--}}
-
-{{--<h2 class="title-style-1">Our Sponsors <span class="title-under"></span></h2>--}}
-
-{{--<ul class="owl-carousel list-unstyled list-sponsors">--}}
-
-{{--<li> <img src="assets/images/sponsors/bus.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/wikimedia.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/one-world.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/wikiversity.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/united-nations.png" alt=""></li>--}}
-
-{{--<li> <img src="assets/images/sponsors/bus.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/wikimedia.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/one-world.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/wikiversity.png" alt=""></li>--}}
-{{--<li> <img src="assets/images/sponsors/united-nations.png" alt=""></li>--}}
-
-{{--</ul>--}}
-
-{{--</div>--}}
-
-{{--</div> <!-- /.our-sponsors -->--}}
 
 
 
@@ -597,320 +597,3 @@
 
 </body>
 </html>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    {{--<div class="section-home about-us fadeIn animated">--}}
-
-        {{--<div class="container">--}}
-
-            {{--<div class="row">--}}
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="about-us-col">--}}
-
-                        {{--<div class="col-icon-wrapper">--}}
-                            {{--<img src="assets/images/icons/our-mission-icon.png" alt="">--}}
-                        {{--</div>--}}
-                        {{--<h3 class="col-title">our mission</h3>--}}
-                        {{--<div class="col-details">--}}
-
-                            {{--<p>Lorem ipsum dolor sit amet consect adipisscin elit proin vel lectus ut eta esami vera dolor sit amet consect</p>--}}
-
-                        {{--</div>--}}
-                        {{--<a href="#" class="btn btn-primary"> Read more </a>--}}
-
-                    {{--</div>--}}
-
-                {{--</div>--}}
-
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="about-us-col">--}}
-
-                        {{--<div class="col-icon-wrapper">--}}
-                            {{--<img src="assets/images/icons/make-donation-icon.png" alt="">--}}
-                        {{--</div>--}}
-                        {{--<h3 class="col-title">Make donations</h3>--}}
-                        {{--<div class="col-details">--}}
-
-                            {{--<p>Lorem ipsum dolor sit amet consect adipisscin elit proin vel lectus ut eta esami vera dolor sit amet consect</p>--}}
-
-                        {{--</div>--}}
-                        {{--<a href="#" class="btn btn-primary"> Read more </a>--}}
-
-                    {{--</div>--}}
-
-                {{--</div>--}}
-
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="about-us-col">--}}
-
-                        {{--<div class="col-icon-wrapper">--}}
-                            {{--<img src="assets/images/icons/help-icon.png" alt="">--}}
-                        {{--</div>--}}
-                        {{--<h3 class="col-title">Help & support</h3>--}}
-                        {{--<div class="col-details">--}}
-
-                            {{--<p>Lorem ipsum dolor sit amet consect adipisscin elit proin vel lectus ut eta esami vera dolor sit amet consect</p>--}}
-
-                        {{--</div>--}}
-                        {{--<a href="#" class="btn btn-primary"> Read more </a>--}}
-
-                    {{--</div>--}}
-
-                {{--</div>--}}
-
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="about-us-col">--}}
-
-                        {{--<div class="col-icon-wrapper">--}}
-                            {{--<img src="assets/images/icons/programs-icon.png" alt="">--}}
-                        {{--</div>--}}
-                        {{--<h3 class="col-title">our programs</h3>--}}
-                        {{--<div class="col-details">--}}
-
-                            {{--<p>Lorem ipsum dolor sit amet consect adipisscin elit proin vel lectus ut eta esami vera dolor sit amet consect</p>--}}
-
-                        {{--</div>--}}
-                        {{--<a href="#" class="btn btn-primary"> Read more </a>--}}
-
-                    {{--</div>--}}
-
-                {{--</div>--}}
-
-
-
-            {{--</div>--}}
-
-        {{--</div>--}}
-
-    {{--</div> <!-- /.about-us -->--}}
-
-    {{--<div class="section-home home-reasons">--}}
-
-        {{--<div class="container">--}}
-
-            {{--<div class="row">--}}
-
-                {{--<div class="col-md-6">--}}
-
-                    {{--<div class="reasons-col animate-onscroll fadeIn">--}}
-
-                        {{--<img src="assets/images/reasons/we-fight-togother.jpg" alt="">--}}
-
-                        {{--<div class="reasons-titles">--}}
-
-                            {{--<h3 class="reasons-title">We fight together</h3>--}}
-                            {{--<h5 class="reason-subtitle">We are humans</h5>--}}
-
-                        {{--</div>--}}
-
-                        {{--<div class="on-hover hidden-xs">--}}
-
-                            {{--<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>--}}
-
-
-                            {{--<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>--}}
-
-                            {{--<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>--}}
-
-                        {{--</div>--}}
-                    {{--</div>--}}
-
-                {{--</div>--}}
-
-
-                {{--<div class="col-md-6">--}}
-
-                    {{--<div class="reasons-col animate-onscroll fadeIn">--}}
-
-                        {{--<img src="assets/images/reasons/we-care-about.jpg" alt="">--}}
-
-                        {{--<div class="reasons-titles">--}}
-
-                            {{--<h3 class="reasons-title">WE care about others</h3>--}}
-                            {{--<h5 class="reason-subtitle">We are humans</h5>--}}
-
-                        {{--</div>--}}
-
-                        {{--<div class="on-hover hidden-xs">--}}
-
-                            {{--<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>--}}
-
-
-                            {{--<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>--}}
-
-                            {{--<p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur praesentium, itaque facilis nesciunt ab omnis cumque similique ipsa veritatis perspiciatis, harum ad at nihil molestias, dignissimos sint consequuntur. Officia, fuga.</p>--}}
-
-                        {{--</div>--}}
-
-
-                    {{--</div>--}}
-
-                {{--</div>--}}
-
-
-            {{--</div>--}}
-
-
-
-        {{--</div>--}}
-
-
-    {{--</div> <!-- /.home-reasons -->--}}
-
-    {{--<div class="section-home our-causes animate-onscroll fadeIn">--}}
-
-        {{--<div class="container">--}}
-
-            {{--<h2 class="title-style-1">Our Causes <span class="title-under"></span></h2>--}}
-
-            {{--<div class="row">--}}
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="cause">--}}
-
-                        {{--<img src="assets/images/causes/cause-hunger.jpg" alt="" class="cause-img">--}}
-
-                        {{--<div class="progress cause-progress">--}}
-                            {{--<div class="progress-bar" role="progressbar" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100" style="width: 30%;">--}}
-                                {{--10$ / 500$--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<h4 class="cause-title"><a href="#">HUNGER AND POVERTY </a></h4>--}}
-                        {{--<div class="cause-details">--}}
-                            {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.--}}
-                        {{--</div>--}}
-
-                        {{--<div class="btn-holder text-center">--}}
-
-                            {{--<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>--}}
-
-                        {{--</div>--}}
-
-
-
-                    {{--</div> <!-- /.cause -->--}}
-
-                {{--</div>--}}
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="cause">--}}
-
-                        {{--<img src="assets/images/causes/cause-education.jpg" alt="" class="cause-img">--}}
-
-                        {{--<div class="progress cause-progress">--}}
-                            {{--<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">--}}
-                                {{--400$ / 700$--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<h4 class="cause-title"><a href="#">EDUCATION AND TRAINING</a></h4>--}}
-                        {{--<div class="cause-details">--}}
-                            {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.--}}
-                        {{--</div>--}}
-
-                        {{--<div class="btn-holder text-center">--}}
-
-                            {{--<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>--}}
-
-                        {{--</div>--}}
-
-
-
-                    {{--</div> <!-- /.cause -->--}}
-
-                {{--</div>--}}
-
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="cause">--}}
-
-                        {{--<img src="assets/images/causes/cause-rights.jpg" alt="" class="cause-img">--}}
-
-                        {{--<div class="progress cause-progress">--}}
-                            {{--<div class="progress-bar" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%;">--}}
-                                {{--400$ / 1000$--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<h4 class="cause-title"><a href="#">HUMAN RIGHTS</a></h4>--}}
-                        {{--<div class="cause-details">--}}
-                            {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.--}}
-                        {{--</div>--}}
-
-                        {{--<div class="btn-holder text-center">--}}
-
-                            {{--<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>--}}
-
-                        {{--</div>--}}
-
-
-
-                    {{--</div> <!-- /.cause -->--}}
-
-                {{--</div>--}}
-
-                {{--<div class="col-md-3 col-sm-6">--}}
-
-                    {{--<div class="cause">--}}
-
-                        {{--<img src="assets/images/causes/cause-culture.jpg" alt="" class="cause-img">--}}
-
-                        {{--<div class="progress cause-progress">--}}
-                            {{--<div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">--}}
-                                {{--400$ / 700$--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-
-                        {{--<h4 class="cause-title"><a href="#">ARTS AND CULTURE </a></h4>--}}
-                        {{--<div class="cause-details">--}}
-                            {{--Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at eros rutrum turpis viverra elementum semper quis ex. Donec lorem nulla, aliquam quis neque vel, maximus lacinia urna.--}}
-                        {{--</div>--}}
-
-                        {{--<div class="btn-holder text-center">--}}
-
-                            {{--<a href="#" class="btn btn-primary" data-toggle="modal" data-target="#donateModal"> DONATE NOW</a>--}}
-
-                        {{--</div>--}}
-
-
-
-                    {{--</div> <!-- /.cause -->--}}
-
-                {{--</div>--}}
-
-            {{--</div>--}}
-
-        {{--</div>--}}
-
-    {{--</div> <!-- /.our-causes -->--}}
-
-
-
-
-
-
-
