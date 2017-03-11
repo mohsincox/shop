e41ff0dab4b775df954141893800fe5b04a0d9e6
Mohsin Iqbal <mohsincse2015@gmail.com>
2017-03-11 18:04:09 +0600

    scrollTop

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,6 +19,26 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="js/jquery-1.12.3.min.js"></script>
 
+    <style>
+        #myBtn {
+          display: none;
+          position: fixed;
+          bottom: 20px;
+          right: 30px;
+          z-index: 99;
+          border: none;
+          outline: none;
+          background-color: green;
+          color: white;
+          cursor: pointer;
+          padding: 15px;
+          border-radius: 10px;
+        }
+
+        #myBtn:hover {
+          background-color: blue;
+        }
+</style>
 
 </head>
 
@@ -26,7 +46,7 @@
 <span class="hidden" id="url">
  {{ url('/') }}
 </span>
-
+<button onclick="topFunction()" id="myBtn" title="Go to top" class="fa fa-arrow-up"></button>
 
 @include('layouts.partial.home_page_menu')
  <!-- /. main-header -->
@@ -965,6 +985,24 @@
         ga('create','UA-XXXXX-X');ga('send','pageview');
     </script>
 
+    <script>
+        // When the user scrolls down 20px from the top of the document, show the button
+        window.onscroll = function() {scrollFunction()};
+
+        function scrollFunction() {
+            if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+                document.getElementById("myBtn").style.display = "block";
+            } else {
+                document.getElementById("myBtn").style.display = "none";
+            }
+        }
+
+        // When the user clicks on the button, scroll to the top of the document
+        function topFunction() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    </script>
 
 </body>
 </html>
